e40c3013bdd4f8d1256354d1f1751032fec16686
Lucas12138 <zizhel@andrew.cmu.edu>
2018-09-09 22:57:04 -0400

    Remove unused package

--- hw2/src/Welcome.java
+++ hw2/src/Welcome.java
@@ -1,11 +1,7 @@
 
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
-import java.util.Enumeration;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
